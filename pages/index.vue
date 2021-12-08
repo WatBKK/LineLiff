147407b122e6070c5c96e382133f3194e385be5d
Thanawat Bekaku <51658544+WatBKK@users.noreply.github.com>
2021-12-08 15:03:59 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+<template>
+  <Tutorial />
+</template>
+
+<script>
+import packageJson from '../package.json'
+export default {
+  data() {
+    return {
+      version: packageJson.version,
+      sdkVersion: '',
+      profile: '',
+      liffError: '',
+    }
+  },
+  mounted() {
+    this.$liffInit
+      .then(() => {
+        // eslint-disable-next-line no-undef
+        if (liff.isLoggedIn()) {
+          this.getUserProfile()
+        } else {
+          // eslint-disable-next-line no-undef
+          liff.login()
+        }
+      })
+      .catch((error) => {
+        this.liffError = error
+      })
+  },
+  methods: {
+    getUserProfile() {},
+  },
+}
+</script>
